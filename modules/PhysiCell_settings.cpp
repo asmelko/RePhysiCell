bc573b245c69a11089d4e3e5606e59800ffb2616
Adam Smelko <adamsmelko1@gmail.com>
2025-10-18 08:59:49 +0200

    Conditionally rely on filesystem header if on windows

--- modules/PhysiCell_settings.cpp
+++ modules/PhysiCell_settings.cpp
@@ -64,6 +64,10 @@
 #                                                                             #
 ###############################################################################
 */
+
+#if defined(_WIN32) || defined(_WIN64)
+#include <filesystem>
+#endif
  
 #include <sys/stat.h>
 #include "./PhysiCell_settings.h"
@@ -358,12 +362,24 @@ bool create_directories(const std::string &path)
 
 bool create_directory(const std::string &path)
 {
-#if defined(__MINGW32__) || defined(__MINGW64__)
-	bool success = mkdir(path.c_str()) == 0;
+#if defined(_WIN32) || defined(_WIN64)
+		// Create the directory using C++17 filesystem library
+	std::error_code ec; // To capture any error
+	if (std::filesystem::create_directories(path, ec)) {
+		return true; // Directory created successfully
+	} else if (ec) {
+		std::cerr << "Error creating directory " << path << ": " << ec.message() << std::endl;
+		return false; // An error occurred
+	}
+	return true; // Directory already exists
 #else
+	#if defined(__MINGW32__) || defined(__MINGW64__)
+	bool success = mkdir(path.c_str()) == 0;
+	#else
 	bool success = mkdir(path.c_str(), 0755) == 0;
-#endif
+	#endif
 	return success || errno == EEXIST;
+#endif
 }
 
 void create_output_directory(const std::string& path)
